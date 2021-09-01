0772ca59692b16b762079f11e8951a5eb1b79da6
Will Westrich <wrwestrich@gmail.com>
2021-09-01 15:16:58 -0500

    Add ability to print to the screen + safety stuffs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,20 @@
 #![no_std]
 #![no_main]
 
-static HELLO: &[u8] = b"Hello World!";
-
 use core::panic::PanicInfo;
+
+mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
+  loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+  print!("Hello World!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
-    loop {}
+  loop {}
 }
